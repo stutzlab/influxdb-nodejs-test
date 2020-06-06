654b118f109cd77d588e9f93600fc100f14f58c0
flaviostutz <flaviostutz@gmail.com>
2020-06-06 17:53:53 -0300

    rounding host label

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ console.log("Using bucketId=" + bucket + "; token=" + token)
 const writeApi = client.getWriteApi(org, bucket)
 
 for(var i=0; i<200; i++) {
-    const data = 'mem,host=host'+ getRandomArbitrary(0,3) +' used_percent=' + getRandomArbitrary(0,100) // Line protocol string
+    const data = 'mem,host=host'+ Math.round(getRandomArbitrary(1,5)) +' used_percent=' + getRandomArbitrary(0,100) // Line protocol string
     writeApi.writeRecord(data)
     console.log(i)
 } 
