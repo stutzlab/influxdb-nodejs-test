4ac72ed1cc4817d15da1cbadb31e61908faaffa1
tiagostutz <tiagostutz@gmail.com>
2020-08-14 15:28:45 -0300

    feat: testing set-output

--- .github/workflows/file-size-impact/report-file-size-impact.js
+++ .github/workflows/file-size-impact/report-file-size-impact.js
@@ -1,5 +1,14 @@
 const { reportFileSizeImpact, readGithubWorkflowEnv } = require("@jsenv/file-size-impact");
 
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+
+try {
+  await exec.exec(`echo "::set-output name=bundle_size::765Kb"`);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
 reportFileSizeImpact({
   ...readGithubWorkflowEnv(),
   projectDirectoryUrl: process.env.GITHUB_WORKSPACE,
