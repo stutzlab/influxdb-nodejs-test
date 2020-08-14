3537ec954a588b19dca7dcb25a2f821ca8aa399c
tiagostutz <tiagostutz@gmail.com>
2020-08-14 16:04:27 -0300

    feat: senidng output as log

--- .github/workflows/file-size-impact/report-file-size-impact.js
+++ .github/workflows/file-size-impact/report-file-size-impact.js
@@ -1,31 +1,13 @@
-const { reportFileSizeImpact, readGithubWorkflowEnv } = require("@jsenv/file-size-impact");
-
 const core = require("@actions/core");
-// const exec = require("@actions/exec");
 
 async function run() {
   try {
     const previousBundleSize = "811KB";
-    // await exec.exec(`echo "Previous Bundle Size: ${previousBundleSize}"`);
     core.info(`Previous Bundle Size: ${previousBundleSize}`);
-    const newBundleSize = "811KB";
+    const newBundleSize = "708KB";
     core.info(`New Bundle Size: ${newBundleSize}`);
-    // await exec.exec(`echo "New Bundle Size: ${newBundleSize}"`);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 run();
-
-// reportFileSizeImpact({
-//   ...readGithubWorkflowEnv(),
-//   projectDirectoryUrl: process.env.GITHUB_WORKSPACE,
-//   installCommand: "npm install",
-//   buildCommand: "npm run build && rm -rf node_modules",
-//   trackingConfig: {
-//     dist: {
-//       "./dist/**/*": true,
-//       "./dist/**/*.map": false,
-//     },
-//   },
-// });
