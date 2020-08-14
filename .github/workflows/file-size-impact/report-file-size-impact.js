f42735c000ea9572f2515a1409b76bbf2fe3a081
tiagostutz <tiagostutz@gmail.com>
2020-08-14 15:38:05 -0300

    fix: add async func to run the test. remove js-file-size

--- .github/workflows/file-size-impact/report-file-size-impact.js
+++ .github/workflows/file-size-impact/report-file-size-impact.js
@@ -3,21 +3,24 @@ const { reportFileSizeImpact, readGithubWorkflowEnv } = require("@jsenv/file-siz
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
-try {
-  await exec.exec(`echo "::set-output name=bundle_size::765Kb"`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    await exec.exec(`echo "::set-output name=bundle_size::765Kb"`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+run();
 
-reportFileSizeImpact({
-  ...readGithubWorkflowEnv(),
-  projectDirectoryUrl: process.env.GITHUB_WORKSPACE,
-  installCommand: "npm install",
-  buildCommand: "npm run build && rm -rf node_modules",
-  trackingConfig: {
-    dist: {
-      "./dist/**/*": true,
-      "./dist/**/*.map": false,
-    },
-  },
-});
+// reportFileSizeImpact({
+//   ...readGithubWorkflowEnv(),
+//   projectDirectoryUrl: process.env.GITHUB_WORKSPACE,
+//   installCommand: "npm install",
+//   buildCommand: "npm run build && rm -rf node_modules",
+//   trackingConfig: {
+//     dist: {
+//       "./dist/**/*": true,
+//       "./dist/**/*.map": false,
+//     },
+//   },
+// });
